d549e97a05e2b48aed8f2ec4465b869997306a6b
Svgta <svgta@live.com>
2021-11-19 17:48:49 +0100

    ...

--- src/Exception.php
+++ src/Exception.php
@@ -3,7 +3,7 @@ namespace svgta\oidc;
 
 class Exception extends \Exception{
   private $info;
-  public fuction __construct($message, $info = null, $code = 0, Throwable $previous = null){
+  public function __construct($message, $info = null, $code = 0, Throwable $previous = null){
     $msg = [
       "message" => $message,
       "info" => $info,
