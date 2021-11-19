a7582916dc12f3336d546199b35c19b440aa361e
Svgta <svgta@live.com>
2021-11-19 17:47:22 +0100

    update

--- src/Exception.php
+++ src/Exception.php
@@ -8,6 +8,6 @@ class Exception extends \Exception{
       "message" => $message,
       "info" => $info,
     ];
-    parent::__construct($message, $code, $previous);
+    parent::__construct($msg, $code, $previous);
   }
 }
